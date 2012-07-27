f3b1c6f151f6b65fb280f9fb12950a3bc609d8ed
Markiyan Matsekh <mr.matsekh@gmail.com>
2012-07-27 20:35:34 +0300

    created project

---
+++ Tetris/Tetris.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetris.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello tetris, lets do it!");
+        }
+    }
+}
