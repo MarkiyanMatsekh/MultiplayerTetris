faf33d05078b659aa31c60ea6fe3dd58a8f94a9e
Markiyan Matsekh <mr.matsekh@gmail.com>
2012-07-27 21:27:29 +0300

    implemented console renderer, copied few interfaces from eleks contracts

--- Tetris/Tetris.ConsoleApp/Program.cs
+++ Tetris/Tetris.ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Tetris.Contracts;
 
 namespace Tetris.ConsoleApp
 {
@@ -9,7 +10,16 @@ namespace Tetris.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello tetris, lets do it!");
+            var rend = new ConsoleRenderer();
+            var sprite = new Sprite(new Dictionary<byte, Color> {{0, Color.Transparent}, {1, Color.Red}},
+                                    new byte[,]{
+                                                   {0, 1, 0},
+                                                   {0, 1, 0},
+                                                   {0, 1, 0},
+                                                   {0, 0, 0},
+                                                   {0, 1, 0}
+                                               }.Transpose());
+            rend.Render(sprite, new Offset(3,3));
         }
     }
 }
