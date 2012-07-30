0aec6f03dc237b1b589c4cc17b8fd6c54110c696
Markiyan Matsekh <mr.matsekh@gmail.com>
2012-07-30 06:59:24 +0300

    implemented simple websockets server handshaking; added referece to Fleck

---
+++ Tetris/Tetris.WebApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using Fleck;
+using Tetris.WebApp;
+
+class Program
+{
+    static void Main()
+    {
+        FleckLog.Level = LogLevel.Debug;
+        var allSockets = new List<IWebSocketConnection>();
+        var server = new Fleck.WebSocketServer("ws://localhost:8080/websession");
+        server.Start(socket =>
+        {
+            socket.OnOpen = () =>
+            {
+                Console.WriteLine("Open!");
+                allSockets.Add(socket);
+            };
+            socket.OnClose = () =>
+            {
+                Console.WriteLine("Close!");
+                allSockets.Remove(socket);
+            };
+            socket.OnMessage = message =>
+            {
+                Console.WriteLine(message);
+                allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+            };
+        });
+
+        var input = Console.ReadLine();
+        while (input != "exit")
+        {
+            foreach (var socket in allSockets.ToList())
+            {
+                socket.Send(input);
+            }
+            input = Console.ReadLine();
+        }
+
+        //StupidWebSocketServer.Start();
+
+        //var wss = new WebSocketServer(8080,
+        //                      "file://",
+        //                      "ws://localhost:8080/websession");
+        //wss.Start();
+
+        //Console.ReadLine();
+    }
+}
---
+++ Tetris/Tetris.WebApp/StupidWebSocketServer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Tetris.WebApp
+{
+    internal static class StupidWebSocketServer
+    {
+        public static void Start()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 8080);
+            listener.Start();
+            while (true)
+            {
+                using (var client = listener.AcceptTcpClient())
+                using (var stream = client.GetStream())
+                {
+                    var headers = new Dictionary<string, string>();
+                    string line = String.Empty;
+                    while ((line = ReadLine(stream)) != String.Empty)
+                    {
+                        var tokens = line.Split(new char[] { ':' }, 2);
+                        if (!String.IsNullOrWhiteSpace(line) && tokens.Length > 1)
+                        {
+                            headers[tokens[0]] = tokens[1].Trim();
+                        }
+                    }
+
+                    var magicString = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+                    var key = headers["Sec-WebSocket-Key"];
+
+                    var acceptKey = CalculateAcceptKey(key + magicString, new ASCIIEncoding());
+
+                    var response =
+                        "HTTP/1.1 101 Switching Protocols" + Environment.NewLine +
+                        "Upgrade: websocket" + Environment.NewLine +
+                        "Connection: Upgrade" + Environment.NewLine +
+                        "Sec-WebSocket-Accept: " + acceptKey + Environment.NewLine +
+                        Environment.NewLine;
+
+                    var bufferedResponse = Encoding.UTF8.GetBytes(response);
+                    stream.Write(bufferedResponse, 0, bufferedResponse.Length);
+
+
+                    byte[] buffer = new byte[4096];
+                    var reader = new BinaryReader(stream);
+                    var writer = new BinaryWriter(stream);
+                    while (true)
+                    {
+                        var bytesRead = reader.Read(buffer, 0, 4096);
+
+                        var str = Encoding.UTF8.GetString(buffer,0,bytesRead);
+
+                        //writer.Write("you send me this: " + str);
+                    }
+
+                }
+            }
+        }
+
+        private static string ReadLine(Stream stream)
+        {
+            var sb = new StringBuilder();
+            var buffer = new List<byte>();
+            while (true)
+            {
+                buffer.Add((byte)stream.ReadByte());
+                var line = Encoding.ASCII.GetString(buffer.ToArray());
+                if (line.EndsWith(Environment.NewLine))
+                {
+                    return line.Substring(0, line.Length - 2);
+                }
+            }
+        }
+
+        private static string CalculateAcceptKey(string text, Encoding enc)
+        {
+            byte[] buffer = enc.GetBytes(text);
+            var cryptoTransformSHA1 = new SHA1CryptoServiceProvider();
+            var hash = cryptoTransformSHA1.ComputeHash(buffer);
+
+            string returnValue = Convert.ToBase64String(hash);
+            return returnValue;
+        }
+
+    }
+}
